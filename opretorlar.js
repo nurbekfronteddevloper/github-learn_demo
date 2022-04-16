b094443fef0a28b6c4575c24d76c72d1f23f487f
nurbekfronteddevloper <nurbeknormatov121@gmail.com>
2022-04-17 04:43:33 +0500

    first commit

---
+++ opretorlar.js
@@ -0,0 +1,26 @@
+// 'use strict '
+// const x = 12,
+//   y = 4;
+
+// console.log(x - y);
+// console.log(x + y);
+// console.log(x * y);
+// console.log(y / x);
+// console.log("hello " + "" + "wordl");
+// const num = 4 + "radio"
+// console.log(4 + "    radio");
+// let z = 10;
+// console.log(++z);
+// console.log(z)
+// let inc = 99;
+// let decr = 99;
+// console.log(++inc);
+// console.log(--decr);
+// let q = 24;
+// let p = 12;
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isClose && isChecked);
+console.log(isClose || isChecked);
